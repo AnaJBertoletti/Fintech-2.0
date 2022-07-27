b6d930dd9e3901cf2c11eaaad045eb9dee86bc6e
Ana Bertoletti <106340047+AnaJBertoletti@users.noreply.github.com>
2022-07-26 22:19:48 -0300

    Add files via upload

---
+++ first.js
@@ -0,0 +1,13 @@
+function typeWrite(e) {
+    const textoArray = e.innerHTML.split('');
+    e.innerHTML = ' ';
+    textoArray.forEach(function (letter, i) {
+        setTimeout(function () {
+            e.innerHTML += letter;
+        }, 80 * i);
+    });
+}
+
+const phrase = document.querySelector('p');
+
+typeWrite(phrase);
